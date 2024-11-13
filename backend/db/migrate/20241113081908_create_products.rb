a152a3a256c496e7fbb9627a472404e4d13df959
marcelarie <googlillo@gmail.com>
2024-11-13 09:33:59 +0100

    update db

--- backend/db/migrate/20241113081908_create_products.rb
+++ backend/db/migrate/20241113081908_create_products.rb
@@ -1,10 +1,9 @@
 class CreateProducts < ActiveRecord::Migration[7.2]
   def change
     create_table :products do |t|
-      t.string :name
+      t.string :name, null: false
       t.text :description
-      t.string :price
-      t.string :decimal
+      t.decimal :price, null: false, precision: 10, scale: 2
 
       t.timestamps
     end
